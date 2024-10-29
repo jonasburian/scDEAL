b3535ec46ca63a8854fda13223781c895955d5c1
Jonas Burian <jonas.burian@protonmail.com>
2024-10-29 15:03:12 +0200

    Change only torch seeds

--- scmodel.py
+++ scmodel.py
@@ -57,9 +57,9 @@ def run_main(args):
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     #from transformers import *
-    random.seed(seed)
-    np.random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(42)
+    np.random.seed(42)
+    os.environ['PYTHONHASHSEED'] = str(42)
 
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark=False
